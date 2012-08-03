00d0062b4c425dab8eb31f73b3515ea7863279a3
Nathan Seidle <nathan@sparkfun.com>
2012-08-02 23:29:22 -0600

    Updates to library. Added example shield layout with 1" 4 digit display.

--- SevSeg/Example/SevSeg_Counter/SevSeg_Counter.ino
+++ SevSeg/Example/SevSeg_Counter/SevSeg_Counter.ino
@@ -5,18 +5,30 @@
  
  This code is originally based Dean Reading's Library deanreading@hotmail.com
  http://arduino.cc/playground/Main/SevenSegmentLibrary
- He didn't have a license on it so I hope he doesn't mind me making it public domain:
- 
+ He didn't have a license on it so I hope he doesn't mind me making it public domain: 
  This code is public domain but you buy me a beer if you use this and we meet someday (Beerware license).
  
- This example is a centi-second counter to demonstrate the use of the SevSeg library.
+ This example is a centi-second counter to demonstrate the use of the SevSeg library. To light
+ the display you have to call sevseg.DisplayNumber(#, decimalPlace) multiple times a second. Put this
+ in the main loop.
 
- I have a cheap one from China, and the pins of the display are in the following order:
- Top Row
- 1,a,f,2,3,b
- Bottom Row
- e,d,dp,c,g,4
- Where the digit pins are 1-4 and the segment pins are a-g + dp
+ SparkFun has a large, 1" 7-segment display that has four digits. 
+ Looking at the display like this: 8.8.8. pin 1 is on the lower row, starting from the left. 
+ Pin 12 is the top row, upper left pin.
+ 
+ Pinout:
+ 1: Segment E
+ 2: Segment D
+ 3: Segment DP
+ 4: Segment C
+ 5: Segment G
+ 6: Digit 4
+ 7: Segment B
+ 8: Digit 3
+ 9: Digit 2
+ 10: Segment F
+ 11: Segment A
+ 12: Digit 1
  
  ToDo:
  Allow for user selectable digits
@@ -39,29 +51,29 @@ SevSeg sevseg;
 
 //Create global variables
 unsigned long timer;
-int CentSec = 0;
+int deciSecond = 0;
 
 void setup() {
 
-  int displayType = COMMON_CATHODE;
+  int displayType = COMMON_CATHODE; //Your display is either common cathode or common anode
   
   //Declare what pins are connected to the digits
-  int digit1 = 2; //Pin 12 on 4 digit display
-  int digit2 = 3; //Pin 9 on 4 digit display
-  int digit3 = 4; //Pin 8 on 4 digit display
-  int digit4 = 5; //Pin 6 on 4 digit display
+  int digit1 = 2; //Pin 12 on my 4 digit display
+  int digit2 = 3; //Pin 9 on my 4 digit display
+  int digit3 = 4; //Pin 8 on my 4 digit display
+  int digit4 = 5; //Pin 6 on my 4 digit display
   
   //Declare what pins are connected to the segments
-  int segA = 6; //Pin 11 on 4 digit display
-  int segB = 7; //Pin 7 on 4 digit display
-  int segC = 8; //Pin 4 on 4 digit display
-  int segD = 9; //Pin 2 on 4 digit display
-  int segE = 10; //Pin 1 on 4 digit display
-  int segF = 11; //Pin 10 on 4 digit display
-  int segG = 12; //Pin 5 on 4 digit display
-  int segDP= 13; //Pin 3 on 4 digit display
+  int segA = 6; //Pin 11 on my 4 digit display
+  int segB = 7; //Pin 7 on my 4 digit display
+  int segC = 8; //Pin 4 on my 4 digit display
+  int segD = 9; //Pin 2 on my 4 digit display
+  int segE = 10; //Pin 1 on my 4 digit display
+  int segF = 11; //Pin 10 on my 4 digit display
+  int segG = 12; //Pin 5 on my 4 digit display
+  int segDP= 13; //Pin 3 on my 4 digit display
   
-  int numberOfDigits = 4;
+  int numberOfDigits = 4; //Do you have a 2 or 4 digit display?
 
   sevseg.Begin(displayType, numberOfDigits, digit1, digit2, digit3, digit4, segA, segB, segC, segD, segE, segF, segG, segDP);
 
@@ -70,21 +82,14 @@ void setup() {
 
 void loop() {
   //Produce an output on the display
-  sevseg.DisplayNumber(CentSec, 2);
+  sevseg.DisplayNumber(deciSecond, 3); //(numberToDisplay, decimal point location)
 
   //Check if 10ms has elapsed
-  unsigned long mils = millis();
-  
-  if (mils - timer >= 10) {
-    timer = mils;
-    CentSec++;
-
-    //Update the number to be displayed, with a decimal
-    //place in the correct position.
-    //sevseg.NewNumber(CentSec, (byte)2);
+  if (millis() - timer >= 100) {
+    timer = millis();
+    deciSecond++;
   }
+  
+  delay(5);
+  
 }
-
-
-
-
